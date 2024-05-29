9cf8a9e1da5cd9154ec17fad59453bf9162e95d7
singas0ng <juliachaewon@naver.com>
2024-05-29 16:35:48 +0900

    경로수정
    - properties 들어있는 파일명 수정

--- src/main/java/kh/mclass/shushoong/config/AppConfig.java
+++ src/main/java/kh/mclass/shushoong/config/AppConfig.java
@@ -8,17 +8,19 @@ import org.springframework.context.annotation.PropertySource;
 import com.siot.IamportRestClient.IamportClient;
 
 @Configuration
-@PropertySource("classpath:/keyproperties/apikey.properties")
+@PropertySource("classpath:/keyfiles/apikey.properties")
 public class AppConfig {
+	
 	//PortOne
 	@Value("${portone.rest.api.key}")
 	private String portoneRestApiKey;
+	
 	@Value("${portone.rest.api.secret.key}")
 	private String portoneRestApiSecretKey;
 	
-	@Bean
+	@Bean //이거 다른 클래스에서 끌어다 사용하고 싶으면 @RequiredArgsConstructor 쓰고 변수 선언하면 그 클래스 내에서 사용 가능
 	public IamportClient iamportClient() {
-		System.out.println("=====1");
+		System.out.println("=======결제API======");
 		System.out.println(portoneRestApiKey);
 		System.out.println(portoneRestApiSecretKey);
 		return new IamportClient(portoneRestApiKey, portoneRestApiSecretKey);

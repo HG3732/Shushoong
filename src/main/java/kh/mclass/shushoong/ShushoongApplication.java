4f8b8a2c274e50b275f9082be8e9c4f3483cf1ea
RabbitApril <empres1215@gmail.com>
2024-05-29 13:00:08 +0900

    /keyproperties/apikey.properties 파일 누락으로 일단 막음

--- src/main/java/kh/mclass/shushoong/ShushoongApplication.java
+++ src/main/java/kh/mclass/shushoong/ShushoongApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:/keyproperties/apikey.properties")
+//@PropertySource("classpath:/keyproperties/apikey.properties")
 public class ShushoongApplication {
 
 	public static void main(String[] args) {

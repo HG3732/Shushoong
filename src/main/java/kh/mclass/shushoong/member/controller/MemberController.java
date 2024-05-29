528354c9f3e54e1ac5b2eae7491834008faa68bd
RabbitApril <empres1215@gmail.com>
2024-05-29 15:20:23 +0900

    Security 설정 및 로그인 연동
    수정사항 있을 수 있음. (db미연동으로 수정x)

--- src/main/java/kh/mclass/shushoong/member/controller/MemberController.java
+++ src/main/java/kh/mclass/shushoong/member/controller/MemberController.java
@@ -3,9 +3,14 @@ package kh.mclass.shushoong.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpSession;
+import kh.mclass.shushoong.member.model.domain.MemberEntity;
 import kh.mclass.shushoong.member.model.service.MemberSecurityService;
 
 @Controller
@@ -15,18 +20,16 @@ public class MemberController {
 	@Autowired
 	private MemberSecurityService memberService;
 	
-	// 로그인 페이지 이동
+	
+	// 로그인 페이지로 이동
 	@GetMapping("login")
 	public String login() {
 		return "member/login";
 	}
 	
-	// 로그아웃
-	
-	@RequestMapping("logout")
-	public String logout(HttpSession session) {
-		session.invalidate();
-		return "redirect:/";
+	// 회원가입 페이지로 이동
+	@GetMapping("join")
+	public String join() {
+		return "member/join";
 	}
-	
 }

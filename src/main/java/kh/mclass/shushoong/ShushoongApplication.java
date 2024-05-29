68e61275b9217af452967e870acf3bc9a497df3c
RabbitApril <empres1215@gmail.com>
2024-05-29 13:16:15 +0900

    Spring Security 추가
    보안설정이 다 끝나지 않아 로그인창 제거 처리함

--- src/main/java/kh/mclass/shushoong/ShushoongApplication.java
+++ src/main/java/kh/mclass/shushoong/ShushoongApplication.java
@@ -2,9 +2,10 @@ package kh.mclass.shushoong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 //@PropertySource("classpath:/keyproperties/apikey.properties")
 public class ShushoongApplication {
 

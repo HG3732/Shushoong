232cd8ccb1551101f7340443a34aae662ae711a2
HG3732 <gyrua34@gmail.com>
2024-05-29 15:00:01 +0900

    Hotel main 제작중

--- src/main/java/kh/mclass/shushoong/hotel/controller/HotelListController.java
+++ src/main/java/kh/mclass/shushoong/hotel/controller/HotelListController.java
@@ -1,4 +1,4 @@
-package kh.mclass.shushoong.hotel;
+package kh.mclass.shushoong.hotel.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HotelListController {
 
+	@GetMapping("/hotel/main")
+	public String hotelMain() {
+		return "hotel/hotel_main";
+	}
 	
 	@GetMapping("/hotel/list")
 	public String hotelList() {

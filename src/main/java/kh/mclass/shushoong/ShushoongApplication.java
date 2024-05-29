0a49b778bd61cb34ce5a5cc6c05680038476808d
singas0ng <juliachaewon@naver.com>
2024-05-29 12:38:56 +0900

    결제 api 수정중

--- src/main/java/kh/mclass/shushoong/ShushoongApplication.java
+++ src/main/java/kh/mclass/shushoong/ShushoongApplication.java
@@ -2,8 +2,10 @@ package kh.mclass.shushoong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/keyproperties/apikey.properties")
 public class ShushoongApplication {
 
 	public static void main(String[] args) {

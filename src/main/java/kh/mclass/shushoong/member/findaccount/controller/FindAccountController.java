b2112331212bbf33d4dc2d885f83690a9922d890
RabbitApril <empres1215@gmail.com>
2024-05-31 11:04:06 +0900

    회원가입 메인페이지 CSS 추가

---
+++ src/main/java/kh/mclass/shushoong/member/findaccount/controller/FindAccountController.java
@@ -0,0 +1,25 @@
+package kh.mclass.shushoong.member.findaccount.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FindAccountController {
+	// 회원가입 메인 페이지로 이동
+	@GetMapping("account/find")
+	public String findInfo() {
+		return "member/findhome";
+	}
+
+	// 일반 회원 아이디/비밀번호 찾기 페이지로 이동
+	@GetMapping("find/customer")
+	public String findInfoCustomer() {
+		return "member/findCustomerInfo";
+	}
+
+	// 사업자 회원 아이디/비밀번호 찾기 페이지로 이동
+	@GetMapping("find/business")
+	public String findInfoBusiness() {
+		return "member/findbusinessInfo";
+	}
+}

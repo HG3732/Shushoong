f5460a259cff4e123c5cfc004465145f99efef24
RabbitApril <empres1215@gmail.com>
2024-06-03 11:16:59 +0900

    네이버 로그인 api 추가

--- src/main/java/kh/mclass/shushoong/member/login/naver/NaverController.java
+++ src/main/java/kh/mclass/shushoong/member/login/naver/NaverController.java
@@ -1,5 +1,12 @@
 package kh.mclass.shushoong.member.login.naver;
 
-public class NaverController {
+import org.springframework.stereotype.Controller;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class NaverController {
+	
+	private final NaverApi naverApi;
 }

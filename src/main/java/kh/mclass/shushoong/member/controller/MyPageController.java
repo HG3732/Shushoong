b4f74765d16db6315860549dc5db5b83ed995fdd
RabbitApril <empres1215@gmail.com>
2024-06-05 17:53:01 +0900

    css 수정 + DTO 생성
    잡다함..

---
+++ src/main/java/kh/mclass/shushoong/member/controller/MyPageController.java
@@ -0,0 +1,17 @@
+package kh.mclass.shushoong.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MyPageController {
+	
+	// 회원가입 메인 페이지로 이동
+	@GetMapping("mypage")
+	public String join() {
+		return "mypage/mypageHome";
+	}
+}

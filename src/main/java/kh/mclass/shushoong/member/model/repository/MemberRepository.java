b1be9fad9d5810ecf2ced368ecd5e91c116abee3
RainFox <empres1215@gmail.com>
2024-06-07 19:02:19 +0900

    로그인 서비스 추가

--- src/main/java/kh/mclass/shushoong/member/model/repository/MemberRepository.java
+++ src/main/java/kh/mclass/shushoong/member/model/repository/MemberRepository.java
@@ -1,6 +1,6 @@
 package kh.mclass.shushoong.member.model.repository;
 
-import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,6 +9,20 @@ import kh.mclass.shushoong.member.model.domain.MemberDto;
 
 @Mapper
 public interface MemberRepository {
+	
+	// 로그인 
 	public MemberDto login(String userId);
-	public MemberDto insert(HashMap<String, Object> map);
+	
+	// 로그인 기록
+	public int log(Map<String, Object> map);
+	
+	// 회원가입
+	public Integer join(MemberDto memberDto);
+	
+	// 아이디 중복 확인
+	public Integer Idcheck(String userId);
+	
+	// 비밀번호 재설정 
+	public int resetPwd(Map<String, Object> map);
+	
 }
---
+++ src/main/java/kh/mclass/shushoong/member/model/service/MemberService.java
@@ -0,0 +1,40 @@
+package kh.mclass.shushoong.member.model.service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import kh.mclass.shushoong.member.model.domain.MemberDto;
+import kh.mclass.shushoong.member.model.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+	private final MemberRepository memberRepository;
+
+	// 로그인
+	public MemberDto login(String userId) {
+		return memberRepository.login(userId);
+	}
+
+	// 로그인 기록
+	public int log(Map<String, Object> map) {
+		return memberRepository.log(map);
+	}
+
+	// 회원가입
+	public Integer join(MemberDto memberDto) {
+		return memberRepository.join(memberDto);
+	}
+
+	// 닉네임 중복 여부
+	public Integer existNick(String userId) {
+		return memberRepository.Idcheck(userId);
+	} 
+	
+	// 비밀번호 재설정
+	public int setPwd(Map<String, Object> map) {
+		return memberRepository.resetPwd(map);
+	}
+}

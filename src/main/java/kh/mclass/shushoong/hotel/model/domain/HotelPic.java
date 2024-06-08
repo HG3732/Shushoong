f15fe6fc2e96a0cdfcd919dff0c11f35fb70d2ca
singas0ng <juliachaewon@naver.com>
2024-06-08 19:28:20 +0900

    호텔사진 dto 추가

---
+++ src/main/java/kh/mclass/shushoong/hotel/model/domain/HotelPic.java
@@ -0,0 +1,11 @@
+package kh.mclass.shushoong.hotel.model.domain;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+public class HotelPic {
+	private String hotelPicture;
+}

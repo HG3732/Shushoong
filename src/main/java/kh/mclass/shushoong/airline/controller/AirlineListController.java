28e8549e876867e5b595f38e28ae79e187f1cc3e
singas0ng <juliachaewon@naver.com>
2024-06-10 18:51:45 +0900

    불필요한 import 지우기

--- src/main/java/kh/mclass/shushoong/airline/controller/AirlineListController.java
+++ src/main/java/kh/mclass/shushoong/airline/controller/AirlineListController.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import kh.mclass.shushoong.airline.model.domain.AirlineInfoDto;
 import kh.mclass.shushoong.airline.model.service.AirlineService;

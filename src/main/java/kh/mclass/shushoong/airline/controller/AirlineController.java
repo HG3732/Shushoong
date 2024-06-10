c59d32da476dcb70bfd1959d97a8a7960462e4ef
Pakgitt <qkrgur0316@gmail.com>
2024-06-10 21:31:38 +0900

    AirlineListController -> AirlineController 이름 변경..

--- src/main/java/kh/mclass/shushoong/airline/controller/AirlineController.java
+++ src/main/java/kh/mclass/shushoong/airline/controller/AirlineController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kh.mclass.shushoong.airline.model.domain.AirlineInfoDto;
 import kh.mclass.shushoong.airline.model.service.AirlineService;
@@ -14,13 +15,13 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
-public class AirlineListController {
+public class AirlineController {
 
 	@Autowired
 	private AirlineService service;
 
 	@GetMapping("/airline/list")
-	public String getAirlineInfo(
+	public String airlineInfo(
 			String departLoc,
 			String arrivalLoc,
 			Model md) {
@@ -36,21 +37,27 @@ public class AirlineListController {
 		}
 		return "airline/airline_list";
 	}
-
-//	@GetMapping("/airline/list")
-//	public String getAirList() {
-//		return "airline/airline_list";
-//	}
+	
+	@GetMapping("airline/list_return/ajax")
+    @ResponseBody
+    public List<AirlineInfoDto> getAirlineInfoReturn(
+            @RequestParam String departLoc,
+            @RequestParam String arrivalLoc) {
+        log.info("Received departLoc: {}, arrivalLoc: {}", departLoc, arrivalLoc);
+        List<AirlineInfoDto> airlineReturnData = service.getAirlineInfo(departLoc, arrivalLoc);
+        log.debug("Return controller 왕복편 data : {}", airlineReturnData);
+        return airlineReturnData;
+    }
 
 	// 항공 메인 페이지
 	@GetMapping("/airline/main")
-	public String airlineHome() {
+	public String airlineMain() {
 		return "airline/airline_main";
 	}
 
 	// 항공 메인 페이지
 	@GetMapping("/airline/customer/reserve/pay")
-	public String customerPay() {
+	public String airlinePay() {
 		return "airline/airline_pay";
 	}
 

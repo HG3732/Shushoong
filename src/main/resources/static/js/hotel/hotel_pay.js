14209cf9ec3b14f82c0328181076b0913565cfcc
singas0ng <juliachaewon@naver.com>
2024-06-12 15:44:55 +0900

    호텔 상세페이지 수정
    - JS 파일 분리
    - eventHandler 추가
      -> 약관동의 내용들 아래화살표 누르면약관상세내용 보이게 하기 및 그 반대 케이스 작성

---
+++ src/main/resources/static/js/hotel/hotel_pay.js
@@ -0,0 +1,17 @@
+function arrowDownClickHandler(){
+	console.log($(this).parents().eq(1).next());
+	
+	var downArrow = $(this).attr('src');
+	
+	if(downArrow == '/shushoong/images/down_arrow.png'){
+		$(this).attr("src", "/shushoong/images/up_arrow.png");
+		$(this).parents().eq(1).next().show();
+		$(this).parents().eq(1).next().css('margin-bottom', '20px')
+		
+	} else {
+		$(this).attr("src", "/shushoong/images/down_arrow.png");
+		$(this).parents().eq(1).next().hide();
+	}
+
+}
+

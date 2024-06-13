df1779017a5ba7dd97e69f7ee91296f1125fbe5e
singas0ng <juliachaewon@naver.com>
2024-06-13 10:40:22 +0900

    호텔쪽 관련 sql 작성 및 이름 수정

---
+++ SQL/hotel_shushoong.sql
@@ -0,0 +1,14 @@
+select * from hotel;
+
+select hotel_name, hotel_eng, hotel_address, hotel_call, hotel_check_in, hotel_check_out, hotel_policy, hotel_intro from hotel where hotel_code = #{hotelCode};
+
+select room_cat, hotel_price, room_att from hotel_room where hotel_code = #{hotelCode}	;	
+
+--객실 정보랑 호텔 상세정보랑 합친 table
+select hotel_name, hotel_eng, hotel_address, hotel_call, hotel_check_in, hotel_check_out, hotel_policy, hotel_intro, room_cat, hotel_price, room_att from hotel 
+join hotel_room using(hotel_code);
+
+--펀의시설
+select * from hotel_facility where hotel_code='2OS001';
+
+select hotel_code, hotel_comment  from hotel_review;

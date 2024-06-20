a328e80ae2f4afbcede7c4124940d8a5049a6611
RabbitApril <empres1215@gmail.com>
2024-06-20 15:42:41 +0900

    유효성 체크 작성중
    !) 이메일 인증 관련 버그 발생.

--- shushoong/src/main/java/kh/mclass/shushoong/member/controller/JoinController.java
+++ shushoong/src/main/java/kh/mclass/shushoong/member/controller/JoinController.java
@@ -1,34 +1,39 @@
 package kh.mclass.shushoong.member.controller;
 
-import java.util.List;
-
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import kh.mclass.shushoong.member.model.domain.MemberDto;
-import kh.mclass.shushoong.member.model.service.MemberSecurityService;
+import kh.mclass.shushoong.member.model.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 //@PropertySource("classpath:/keyfiles/apikey.properties")
 public class JoinController {
 	
-	private final MemberSecurityService memberservice;
+	@Autowired
+	private final MemberService memberservice;
 	
 	
 	// 회원가입 메인 페이지로 이동
 	@GetMapping("join")
 	public String join() {
 		return "member/joinHome";
+	}
+	
+	// 아이디 체크
+	@PostMapping("/idcheck.ajax")
+	@ResponseBody
+	public int idCheck(@RequestParam("userId") String userId) {
+		int cnt = memberservice.idCheck(userId);
+		log.info("userId : " + userId);
+		return cnt;
 	}
 
 	// 일반유저 가입 페이지로 이동
--- shushoong/src/main/java/kh/mclass/shushoong/member/model/service/MemberService.java
+++ shushoong/src/main/java/kh/mclass/shushoong/member/model/service/MemberService.java
@@ -12,18 +12,18 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 public class MemberService {
-	
+
 	@Autowired
 	private MemberRepository memberRepository;
-	
+
 	public List<MemberDto> selectAllList() {
 		return memberRepository.selectAllList();
 	}
-	
+
 	public List<MemberDto> selectOne(String userId) {
 		return memberRepository.selectOne(userId);
 	}
-	
+
 	// 로그인
 	public MemberDto login(String userId) {
 		return memberRepository.login(userId);
@@ -34,21 +34,21 @@ public class MemberService {
 		return memberRepository.log(map);
 	}
 
+	// 아이디 중복 여부
+	public int idCheck(String userId) {
+		return memberRepository.idCheck(userId);
+	}
+
 	// 회원가입
 	public int join(MemberDto memberDto) {
 		return memberRepository.join(memberDto);
 	}
-	
+
 	// 아이디 찾기
 	public String findId(Map<String, Object> map) {
 		return memberRepository.findId(map);
 	}
 
-	// 아이디 중복 여부
-	public int idCheck(String userId) {
-		return memberRepository.idCheck(userId);
-	} 
-	
 	// 비밀번호 재설정
 	public String setPwd(Map<String, Object> map) {
 		return memberRepository.resetPwd(map);

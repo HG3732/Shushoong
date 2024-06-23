6d31715d630ad2c409b0e8ac53681f3cf3f69764
RainFox <empres1215@gmail.com>
2024-06-24 00:00:47 +0900

    권한 부여 시도중
    !) ROLE 파일을 못읽어옴.

--- shushoong/src/main/java/kh/mclass/shushoong/member/model/domain/MemberDto.java
+++ shushoong/src/main/java/kh/mclass/shushoong/member/model/domain/MemberDto.java
@@ -1,5 +1,6 @@
 package kh.mclass.shushoong.member.model.domain;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
@@ -11,10 +12,12 @@ public class MemberDto {
 	private String userName;
 	private String userPwd;
 	private String userEmail;
-	private String userGrade;
+	private MemberRole userGrade;
 	private String userStatus;
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
 	private String joinDate;
 	private Integer emailReceive;
 	private Integer msgReceive;
+	@DateTimeFormat(pattern = "yyyy-mm-dd hh:mm:ss")
 	private String latestLogin;
 }

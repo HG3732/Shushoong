0a5e341ed04c0c60089c2fa9aa94196d6248c7b2
RabbitApril <empres1215@gmail.com>
2024-06-24 17:16:55 +0900

    일반유저 코드 추가
    *) 일반유저는 마케팅 수신 유무 코드도 추후 추가

--- shushoong/src/main/java/kh/mclass/shushoong/mypage/customer/model/service/MypageCustomerService.java
+++ shushoong/src/main/java/kh/mclass/shushoong/mypage/customer/model/service/MypageCustomerService.java
@@ -1,15 +1,27 @@
 package kh.mclass.shushoong.mypage.customer.model.service;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kh.mclass.shushoong.member.model.domain.MemberDto;
-import kh.mclass.shushoong.mypage.admin.model.repository.MypageAdminRepository;
+import kh.mclass.shushoong.mypage.customer.model.repository.MypageCustomerRepository;
 
 @Service
 public class MypageCustomerService {
-
+	
+	@Autowired
+	private MypageCustomerRepository mypageRepository;
+	
+	private BCryptPasswordEncoder bcrypt;
+	
+	public MemberDto selectOne(String userId) {
+		return mypageRepository.selectOne(userId);
+	}
+	
+	// 비밀번호 재설정
+	public String resetPwd(MemberDto dto) {
+		dto.setUserPwd(bcrypt.encode(dto.getUserPwd()));
+		return mypageRepository.resetPwd(dto);
+	}
 }

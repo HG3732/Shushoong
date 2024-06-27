e44db0dc5a3a24143b5f8f70f9dd56747349b565
RabbitApril <empres1215@gmail.com>
2024-06-27 15:43:18 +0900

    헤더 로그인 권한 따라 토글 부여

--- shushoong/src/main/java/kh/mclass/shushoong/member/model/service/MemberSecurityService.java
+++ shushoong/src/main/java/kh/mclass/shushoong/member/model/service/MemberSecurityService.java
@@ -15,17 +15,20 @@ import kh.mclass.shushoong.member.model.domain.MemberDto;
 import kh.mclass.shushoong.member.model.domain.MemberRole;
 import kh.mclass.shushoong.member.model.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberSecurityService implements UserDetailsService {
 	
 	private final MemberRepository memberRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+		log.debug("membersecurityservice ==============");
+		
 		Optional<MemberDto> loginDtoOp = Optional.ofNullable(memberRepository.login(userId));
-		System.out.println(loginDtoOp);
 		if(loginDtoOp.isEmpty()) {
 			throw new UsernameNotFoundException("존재하지 않는 아이디입니다.");
 		}
@@ -33,11 +36,10 @@ public class MemberSecurityService implements UserDetailsService {
 		MemberDto loginDto = loginDtoOp.get();
 		List<GrantedAuthority> authorities = new ArrayList<>();
 		switch(loginDto.getUserGrade()) {
-		case "admin" : authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
-		case "customer": authorities.add(new SimpleGrantedAuthority(MemberRole.CUSTOMER.getValue()));
-		case "business":authorities.add(new SimpleGrantedAuthority(MemberRole.BUSINESS.getValue()));
+			case "admin" : authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue())); break;
+			case "customer": authorities.add(new SimpleGrantedAuthority(MemberRole.CUSTOMER.getValue())); break;
+			case "business":authorities.add(new SimpleGrantedAuthority(MemberRole.BUSINESS.getValue())); break;
 		}
-		
 		return new User(loginDto.getUserId(), loginDto.getUserPwd(), authorities);
 	}
 }

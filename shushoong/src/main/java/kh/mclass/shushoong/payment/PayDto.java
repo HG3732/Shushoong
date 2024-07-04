dc462b1127dd646fd1f17ad4b4587a40230c342a
singas0ng <juliachaewon@naver.com>
2024-07-04 23:54:22 +0900

    결제관련  dto 만들기

---
+++ shushoong/src/main/java/kh/mclass/shushoong/payment/PayDto.java
@@ -0,0 +1,19 @@
+package kh.mclass.shushoong.payment;
+
+import org.springframework.stereotype.Component;
+import lombok.Data;
+
+@Data
+@Component
+public class PayDto {
+
+	private String approveNo;
+	private String reserveCorper;
+	private String cardNum;
+	private String payPrice;
+	private String moneyCat;
+	private String payStatus;
+	private String hotelReserveCode;
+	private String airlineReserveCode;
+	
+}

6bbca73b53ac8dcf50ec78dee514cd714979c2f0
HG3732 <gyrua34@gmail.com>
2024-07-05 20:47:52 +0900

    1:1 문의 답변 공백으로 적어도 등록되던 현상 수정

--- shushoong/src/main/resources/static/js/mypage/admin/viewQnA.js
+++ shushoong/src/main/resources/static/js/mypage/admin/viewQnA.js
@@ -2,14 +2,44 @@ $(loadedHandler);
 
 function loadedHandler() {
 	$('.btn-write-answer').on('click', writeAnswer);
+	$('.btn-submit-answer').on('click', submitAnswer);
 }
 
 function writeAnswer(event) {
 	event.preventDefault();
 	$('.no-content').css('display', 'none');
 	$('.write-answer').css('display', 'block');
-	$('.btn-write-answer').off('click', writeAnswer);
-	$('.btn-write-answer').text('작성완료');
-	$('.btn-write-answer').attr('type', 'submit');
+	$('.btn-write-answer').css('display', 'none');
+	$('.btn-submit-answer').css('display', 'block');
+	console.log($('.write-answer').text());
+	/*if($('.write-answer').val() != null || $('.write-answer').val() != '') {
+		$('.btn-write-answer').attr('type', 'submit');
+	} else {
+		alert("답변을 제대로 작성해주세요.");
+		event.preventDefault();
+	}*/
 	event.preventDefault();
+}
+
+function submitAnswer() {
+	var id = $(this).data("faqid");
+	console.log(id);
+	var answer = $('.write-answer').val();
+	if(answer == null || answer == '') {
+		alert("답변을 제대로 작성해주세요.");
+	} else {
+		$.ajax({
+		url: "/shushoong/support/qna/write/answer.ajax",
+		method: "post",
+		data: { faqId : id,
+				ansContent : answer },
+		error: function(xhr, status, error) {
+				console.log('AJAX 실패:', error);
+				}
+		})
+		//success함수 대체
+		.done(function(response){
+			$('.wrapper').replaceWith(response);
+		});
+	}
 }

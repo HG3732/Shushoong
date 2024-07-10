c589de70dfce0e5772478477a7541e920bbcc63d
AIYA9974 <beakseungbum10@gmail.com>
2024-07-10 18:44:22 +0900

    COUNTRY_LOC_PIC 추가

--- shushoong/src/main/resources/static/js/airline/airline_slides.js
+++ shushoong/src/main/resources/static/js/airline/airline_slides.js
@@ -1,9 +1,9 @@
 	
 		
 										/* 저렴한 가격 캐러셀 */
-	cheapIndex = 0;
-	var cheapSlideSize =document.getElementsByClassName("cheap_item");
-	$(".cheap_next_btn").on("click", function(){
+	var cheapIndex = 0;
+	var cheapSlideSize =document.getElementsByClassName("airline-recommend");
+	$(".left.btn2").on("click", function(){
 		if(cheapIndex > cheapSlideSize.length-4){
 			
 		}else{
@@ -12,7 +12,7 @@
 		}
 	});
 	
-	$(".cheap_prev_btn").on("click", function(){
+	$(".right.btn2").on("click", function(){
 		if(cheapIndex <  1){
 			
 		}else{
@@ -23,8 +23,8 @@
 	
 	
 	function cheapSlide(cheapIndex){
-		$(".cheap_item").each(function(){
-			var transval = -320*cheapIndex;
+		$(".airline-recommend").each(function(){
+			var transval = -330*cheapIndex;
 			$(this).css("transform","translateX("+transval+"px)")
 		});
 	};
@@ -34,35 +34,4 @@
 							
 							
 							
-							
-							/* 빠른 도착시간 캐러셀 */
-	
-		fastIndex = 0;
-	var fastSlideSize =document.getElementsByClassName("fast_item");
-	$(".fast_next_btn").on("click", function(){
-		if(fastIndex > fastSlideSize.length-4){
-			
-		}else{
-			fastIndex++;
-			fastSlide(fastIndex);
-		}
-	});
-	
-	$(".fast_prev_btn").on("click", function(){
-		if(fastIndex <  1){
-			
-		}else{
-			fastIndex--;
-			fastSlide(fastIndex);
-		}
-	});
-	
-	
-	function fastSlide(fastIndex){
-		$(".fast_item").each(function(){
-			var transval = -320*fastIndex;
-			$(this).css("transform","translateX("+transval+"px)")
-		});
-	};
-	
 		
